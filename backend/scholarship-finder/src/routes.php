8c7830771d135511600fb582dba88b4eb9afef73
Omar Trejo <oetrejomedina@gmail.com>
2017-11-08 21:10:04 +0000

    Trying to get search working

--- backend/scholarship-finder/src/routes.php
+++ backend/scholarship-finder/src/routes.php
@@ -6,13 +6,15 @@ use Slim\Http\Response;
 // Routes
 
 
+    
 $app->get('/', function (Request $request, Response $response, array $args) {
-    // Sample log message
+   
+    
     $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
+          
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
 
 $app->get('/login', function (Request $request, Response $response, array $args) {
     
@@ -71,3 +73,94 @@ $container['notFoundHandler'] = function ($c) {
             ->write('Page not found');
     };
 };
+
+$app->get( '/search', function (Request $request) use ($app) {
+    //function (Request $request, Response $response, array $args) { // 
+        //$query = $app->request->get('query');
+        //$query = $app->request->getQueryParams();
+        $query = $request->getQueryParams();
+        $hookObject = (object) ['query' => $query, 'results' => []];
+        $app->applyHook('search', $hookObject);
+        $app->render(
+            'search.phtml',
+            [
+                'query' => $query,
+                'results' => $hookObject->results,
+            ]
+        );
+    }
+);
+
+/*
+$app->hook('search', function ($hookObject) use ($posts) {
+    $query = $hookObject->query;
+    $result = [];
+    foreach ($posts as $post) {
+        $content = strip_tags(
+            file_get_contents(__DIR__ . '/../templates/' . $post['slug'] . '.html')
+        );
+        if (stripos($content, $query) !== false || stripos($post['title'], $query) !== false) {
+            $result[] = ['title' => $post['title'], 'uri' => '/{[query]}' . $post['slug']];
+        }
+    }
+    if (count($result)) {
+        $hookObject->results['Results'] = $result;
+    }
+});
+
+$posts = [
+    [
+        'title' => 'Cholesky decomposition in PHP',
+        'created' => new DateTime('2015-01-16'),
+        'slug' => 'cholesky_decomposition_in_php',
+    ],
+    [
+        'title' => 'Calculate the inverse of a matrix in PHP',
+        'created' => new DateTime('2015-01-14'),
+        'slug' => 'calculate_the_inverse_of_a_matrix_in_php',
+    ],
+];
+
+$app->render(
+            'search.phtml',
+            [
+                'query' => $query,
+                'results' => $hookObject->results,
+            ]
+        );
+        
+$app->post('/', 'uploadFile');
+
+function uploadFile () {
+    if (!isset($_FILES['uploads'])) {
+        echo "No files uploaded!!";
+        return;
+    }
+    $imgs = array();
+
+    $files = $_FILES['uploads'];
+    $cnt = count($files['name']);
+
+    for($i = 0 ; $i < $cnt ; $i++) {
+        if ($files['error'][$i] === 0) {
+            $name = uniqid('img-'.date('Ymd').'-');
+            if (move_uploaded_file($files['tmp_name'][$i], 'uploads/' . $name) === true) {
+                $imgs[] = array('url' => '/uploads/' . $name, 'name' => $files['name'][$i]);
+            }
+
+        }
+    }
+
+    $imageCount = count($imgs);
+
+    if ($imageCount == 0) {
+       echo 'No files uploaded!!  <p><a href="/">Try again</a>';
+       return;
+    }
+
+    $plural = ($imageCount == 1) ? '' : 's';
+
+    foreach($imgs as $img) {
+        printf('%s <img src="%s" width="50" height="50" /><br/>', $img['name'], $img['url']);
+    }
+} */
